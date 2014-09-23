ad8242d6680976944fdd8341863857de84e943f6
jahnavi10032@iiitd.ac.in <jahnavi10032@iiitd.ac.in@31ed6152-2a8c-3ea6-4bf7-6695b5901da3>
2014-09-23 07:43:02 +0000

    Admin page: More Added features

---
+++ smartalloc/admin_course_create.php
@@ -0,0 +1,24 @@
+<?php
+include "connect.php";
+
+$c_no = $_POST['i2'];
+$c_name = $_POST['i3'];
+$instructor = $_POST['i4'];
+$enrol_size = $_POST['i5'];
+$ta_reqd = $enrol_size/20;
+$email = $_POST['i6'];
+$contact = $_POST['i7'];
+
+if(!isset($_POST['add']))
+{
+	//echo "Please Fill the form";
+	header("Location: admin_courseinfo.php");	
+}
+else
+{
+	mysql_query("INSERT into course_list(id,c_no,c_name,instructor,enrol_size,ta_reqd,email,contact) VALUES ('','$c_no','$c_name','$instructor','$enrol_size','$ta_reqd','$email','$contact')");
+//	echo "INSERT into course_list(id,c_no,c_name,instructor,enrol_size,ta_reqd,email,contact) VALUES ('','$id','$c_no','$c_name','$instructor','$enrol_size','$ta_reqd','$email','$contact')";
+	//echo "Added!";
+	header("Location: admin_courseinfo.php");
+}
+?>
